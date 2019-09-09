a62d55d2ff3597b9f598ee313d033534153b0eb8
anwar1938anwar <55034228+anwar1938anwar@users.noreply.github.com>
2019-09-09 17:37:52 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("620623749586092045")
+setInterval(function() {
+channel.send(`I ANWAR YOU TUBE GG - DONT BAN - HAAAAAAAHHHAAAAAAAAAAAAAAAAAAAAA`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
